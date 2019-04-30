526a6b26b24d3d383b725258c03d9cd70464bd65
Julien DE NADAI <julien.denadai@gmail.com>
2019-04-30 11:40:49 +0200

    Create a cross-platform project

---
+++ KoinSwift/KoinSwiftIos/KoinSwiftIos.h
@@ -0,0 +1,18 @@
+//
+//  KoinSwiftIos.h
+//  KoinSwiftIos
+//
+//  Created by DE NADAI Julien (i-BP - INFOTEL) on 30/04/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KoinSwiftIos.
+FOUNDATION_EXPORT double KoinSwiftIosVersionNumber;
+
+//! Project version string for KoinSwiftIos.
+FOUNDATION_EXPORT const unsigned char KoinSwiftIosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KoinSwiftIos/PublicHeader.h>
+
+
